595bd6fae472d05e226cc006be7ffc3ede679e48
Amitabh Suyash <56742160+Asy1921@users.noreply.github.com>
2024-10-12 04:52:36 +0530

    Server WIP

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module backend
 go 1.23.1
 
 require (
+	github.com/jackc/pgx v3.6.2+incompatible
 	github.com/jackc/pgx/v5 v5.7.1
 	github.com/stretchr/testify v1.8.1
 )
@@ -12,6 +13,7 @@ require (
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/kr/text v0.2.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rogpeppe/go-internal v1.13.1 // indirect
 	golang.org/x/crypto v0.27.0 // indirect
--- main.go
+++ main.go
@@ -1,16 +1,27 @@
-package sqlc
+package main
 
 import (
+	"backend/db/sqlc"
+	"context"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/jackc/pgx/v5"
 )
-
+	const dbDriver= "postgres"
+	const dbSource="postgresql://postgres:secret@localhost:5432/postgres?sslmode=disable"
 func sayHi(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
 
-	json.NewEncoder(w).Encode("Hello from Go Backed")
+	var store *sqlc.Queries
+	ctx := context.Background()
+	conn,err :=pgx.Connect(ctx, dbSource)
+	store= sqlc.New(conn)
+	account,err:=store.GetAccount(ctx,3)
+	fmt.Println(err.Error())
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(account)
 }
 func main() {
 	fmt.Println("Running on localhost:8080");
